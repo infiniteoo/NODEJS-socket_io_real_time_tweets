54c89e10728326e90d9113cd450f4bb3b59611d1
Troy Dorman <troydorman@gmail.com>
2021-08-09 07:34:31 -0600

    created an async getRules function to query the twitter rules URL.  successfully receiving response

--- server/index.js
+++ server/index.js
@@ -3,10 +3,31 @@ const config = require("dotenv").config();
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 console.log(TOKEN);
 
-const rulesURL = "http://api.twitter.com/2/tweets/search/stream/rules";
+const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
-  "http://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id";
+  "https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id";
 
-  const rules = [{ value: 'giveaway'}]
+const rules = [{ value: "giveaway" }];
 
-  
+// get stream rules
+async function getRules() {
+  const response = await needle("get", rulesURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  console.log(response.body);
+  return response.body;
+}
+
+(async () => {
+  let currentRules;
+
+  try {
+    currentRules = await getRules();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
