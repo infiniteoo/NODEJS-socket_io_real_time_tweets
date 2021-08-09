c652e02abdf8c85fb045e042aca7ed4e1ba25960
Troy Dorman <troydorman@gmail.com>
2021-08-09 07:30:23 -0600

    created reference variables to our dependencies and defined our rules & stream URls

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const needle = require("needle");
+const config = require("dotenv").config();
+const TOKEN = process.env.TWITTER_BEARER_TOKEN;
+console.log(TOKEN);
+
+const rulesURL = "http://api.twitter.com/2/tweets/search/stream/rules";
+const streamURL =
+  "http://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id";
+
+  const rules = [{ value: 'giveaway'}]
+
+  
